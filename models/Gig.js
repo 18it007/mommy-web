e56af44614333fb41c0e03b447e89e9140fd1f5f
Nguyễn Anh Dũng - 18IT1 <44762416+18it007@users.noreply.github.com>
2019-05-20 09:33:35 +0700

    Create Gig.js

---
+++ models/Gig.js
@@ -0,0 +1,42 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+
+const Gig = db.define('gig', {
+  title: {
+    type: Sequelize.STRING
+  },
+  technologies: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.STRING
+  },
+  budget: {
+    type: Sequelize.STRING
+  },
+  contact_email: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = Gig;
+/* Create table user in database
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  phone: {
+    type: Sequelize.STRING
+  },
+  status: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = User;*/
